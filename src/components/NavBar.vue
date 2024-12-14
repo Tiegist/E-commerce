518bba822419901975f695e3d14f6fa847ba077d
Tiegist <tiegistgirma40@gmail.com>
2024-12-14 04:02:41 +0300

    some change on responsiveness

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,16 @@
 <template>
-    <nav class="lg:fixed lg:top-0 lg:w-screen lg:h-16 bg-zinc-800 text-white lg:z-10 fixed top-0 w-screen h-16 z-10 ">
+  <nav class="lg:fixed lg:top-0 lg:w-screen lg:h-16 bg-zinc-800 text-white lg:z-10 fixed top-0 w-screen h-16 z-10 ">
     <div class="lg:flex flex lg:pt-2 pt-2 lg:pl-2 pl-2">
-      <img src="/cartlogo.jpg" class="w-16 pt-1 pl-1 rounded-md lg:w-20 lg:pt-1 lg:pl-1 lg:rounded-lg">
-      <p class="lg:ml-2 lg:mt-3 ml-2 mt-3">E-Commerce</p>
+      <div class="lg:flex flex">
+        <img src="/cartlogo.jpg" class="w-16 pt-1 pl-1 rounded-md lg:w-20 lg:pt-1 lg:pl-1 lg:rounded-lg">
+        <p class="lg:ml-2 lg:mt-3 ml-2 mt-3">E-Commerce</p>
+      </div>
+      <div class="lg:ml-96 lg:mt-3">
+        <div class="lg:ml-32">
+          <input type="text" class="lg:w-72 lg:outline-cyan-600 outline-cyan-600 lg:border-2 lg:border-cyan-800 lg:ml-96 lg:py-2 lg:-mt-2 lg:rounded-2xl">
+          <i class="pi pi-search ml-3"></i>
+        </div>
+      </div>
     </div>
   </nav>
 </template>
@@ -13,6 +21,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/BooksDetail.vue
+++ src/views/BooksDetail.vue
@@ -2,8 +2,8 @@
     <NavBar />
 <div class="mt-16">
    <SideBar />
-   <div class="font-serif mb-40 mt-5">
-    <div v-for="(img, index) in images" :key="index" class="bg-zinc-200 w-80 mt-4 h-4/5 ml-5">
+   <div class="font-serif mb-40 mt-5 lg:flex lg:ml-56 lg:">
+    <div v-for="(img, index) in images" :key="index" class="bg-zinc-200 w-80 mt-4 h-4/5 ml-5 lg:w-72 lg:px-2 py-2">
         <img :src="img.src" alt="" class="w-56 ml-10 pt-2 shadow-lg">
         <p class="text-center text-lg">{{ img.title }}</p>
         <p class="text-center">{{ img.year }}</p>
@@ -12,6 +12,7 @@
         <p class="text-center">{{ img.price }}</p>
     </div>
    </div>
+   
 </div>  
 </template>
 
@@ -30,6 +31,7 @@ data(){
     {src: "/book-kid2.webp", title: "My Magical Choices ",year:"October 30, 2019",auther:"by Becky Cummings (Author), Zuzana Svobodova (Illustrator)",type:"Hardcover", price:"$16"},
     {src: "/book-kid3.webp", title: "Be The One",year:"October 11, 2024",auther:"by Wonder House Books (Author)",type:"Hardcover", price:"$18"},
     {src: "/book-kid4.webp", title: "How Things Work ",year:"2018",auther:"by Jamie Thorne",type:"Paperback", price:"$11"},
+    
 ]
     }
 }
